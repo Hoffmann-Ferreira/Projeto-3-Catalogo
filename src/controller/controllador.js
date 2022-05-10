7a0f13b6136dceaaf968e181f08629e37198d428
Hoffmann-Ferreira <andreihf@outlook.com>
2022-05-09 22:32:27 -0300

    feat: Rotas, CSS e mensagem de cadastro

--- src/controller/controllador.js
+++ src/controller/controllador.js
@@ -45,21 +45,71 @@ export const getApagar = async (req, res) =>{
 };
 
 export const getCadastrar = (req, res) =>{
-  res.render("cadastro.ejs");
+  res.render("cadastro.ejs", {toggle: false});
 
 };
 
 export const postCadastrar = async (req, res) => {
   const {nome, sinopse, genero, ano, imagem, trailer, critica} = req.body
   try {
-    await filmes.create({nome, sinopse, genero, ano, imagem, trailer, critica})
-    res.redirect("/")
+    if(!nome){
+      res.send("O campo nome é obrigatório");
+    }else if(!sinopse){
+      res.send("O campo sinopse é obrigatório");
+    }else if(!genero){
+      res.send("O campo genero é obrigatório");
+    }else if(!ano){
+      res.send("O campo ano é obrigatório");
+    }else if(!imagem){
+      res.send("O campo imagem é obrigatório");
+    }else if(!trailer){
+      res.send("O campo trailer é obrigatório");
+    }else if(!critica){
+      res.send("O campo critica é obrigatório");
+    }else{
+      await filmes.create({nome, sinopse, genero, ano, imagem, trailer, critica})
+      res.render("cadastro.ejs",{ toggle: true})
+    }
 
   } catch (error) {
     res.send(error.message);
 
   }
 
+}
+
+export const getEditar = async (req, res) =>{
+  try{
+    const filmeAtual = await filmes.findByPk(req.params.id)
+
+    res.render("editar.ejs", {
+      filmeAtual
+    });
+  } catch(error){
+    res.send(error.message);
+  }
+}
+export const postEditar = async (req, res) =>{
+  try{
+    console.log(req.body)
+    const { nome, sinopse, genero, ano, imagem, trailer, critica} = req.body;
+    await filmes.update({
+      nome: nome,
+      sinopse: sinopse,
+      genero: genero,
+      ano: ano,
+      imagem: imagem,
+      trailer: trailer,
+      critica: critica,
+    }, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.redirect("/")
+  } catch (error){
+    res.send(error.message)
+  }
 }
 
 
--- src/routers/routers.js
+++ src/routers/routers.js
@@ -4,15 +4,23 @@ import {
     getDetalhes,
     getApagar,
     getCadastrar,
-    postCadastrar
+    postCadastrar,
+    getEditar,
+    postEditar
 
 } from "../controller/controllador.js";
 
 export const routers = express.Router();
 
 routers.get("/", getIndex);
+
 routers.get("/detalhes/:id", getDetalhes);
+
 routers.get("/apagar/:id", getApagar);
+
 routers.get("/cadastrar", getCadastrar);
 routers.post("/cadastrar", postCadastrar);
+
+routers.get("/editar/:id", getEditar);
+routers.post("/editar/:id", postEditar);
 
